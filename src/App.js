b5ba504f237f1a1eb39be9e320efb710f24992dd
mohamedmuqsith <mohamedmukshidh35@gmail.com>
2025-02-24 21:51:50 +0530

    update project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar';
+import Search from "./components/Search";
+import Products from './components/products';
+import About from './components/About';
+import Footer from './components/Footer';
+function App(){
+    return(
+        <>
+    <Navbar></Navbar>
+    <Search></Search>
+    <Products></Products>
+    <About></About>
+    <Footer></Footer>
+    
+</>
+    )
+}
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+//About 
+var Abouttxt1={
+    textAlign:"center"
+}
+var Abouttxt2={
+    marginTop:"20px",
+    textAlighn:"center"
+}
+function About(){
+    return(
+    <div style={Abouttxt1}>
+        <div class="about" style={Abouttxt2}>
+            <h1>About Us</h1>
+            <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+        </div>
+        </div>)
+}
+
+export default About;
